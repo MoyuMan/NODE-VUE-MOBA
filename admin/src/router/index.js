75cfce15ddada6fd06357c3b099ad0e9c51b0aac
MoyuMan <1134536610@qq.com>
2020-08-11 17:58:25 +0800

    p16英雄编辑

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Main from '../views/Main.vue'
+
 import CategoryEdit from '../views/CategoryEdit.vue'
+import CategoryList from '../views/CategoryList.vue'
+
+import ItemEdit from '../views/ItemEdit.vue'
+import ItemList from '../views/ItemList.vue'
+
+import HeroEdit from '../views/HeroEdit.vue'
+import HeroList from '../views/HeroList.vue'
 
 Vue.use(VueRouter)
 
@@ -12,7 +20,18 @@ Vue.use(VueRouter)
     name: 'main',
     component: Main,
     children: [
-      { path: '/categories/create', component: CategoryEdit } 
+      { path: '/categories/create', component: CategoryEdit } ,
+      { path: '/categories/edit/:id', component: CategoryEdit, props: true } ,
+      { path: '/categories/list', component: CategoryList } ,
+
+      { path: '/items/create', component: ItemEdit } ,
+      { path: '/items/edit/:id', component: ItemEdit, props: true } ,
+      { path: '/items/list', component: ItemList } ,
+
+      { path: '/heroes/create', component: HeroEdit } ,
+      { path: '/heroes/edit/:id', component: HeroEdit, props: true } ,
+      { path: '/heroes/list', component: HeroList } 
+
     ]
   },
   
--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ const express = require("express")
 
 const app = express()
 
-// app.use(require('cors')())
 app.use(require('cors')())
 app.use(express.json())
-
+app.use('/uploads', express.static(__dirname + '/uploads'))
 
 require('./plugins/db')(app)
 require('./routes/admin')(app)
